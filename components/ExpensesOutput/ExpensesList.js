7f6bb69ced2a83c195d5b7175462130e940f48d3
korayerdem89 <developerkoray@gmail.com>
2022-10-28 13:18:37 +0300

    expense list  has been shown on the screen

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,6 +1,13 @@
-import { FlatList, View, Text } from "react-native";
-function ExpensesSummary() {
-  <FlatList />;
+import { FlatList, Text } from "react-native";
+
+function renderExpenseItem(itemData) {
+  return <Text>{itemData.item.description}</Text>
 }
 
-export default ExpensesSummary;
+
+
+function ExpensesList({ expenses }) {
+ return <FlatList data={ expenses } renderItem={renderExpenseItem} keyExtractor= {(item) => item.id} />;
+}
+
+export default ExpensesList;
